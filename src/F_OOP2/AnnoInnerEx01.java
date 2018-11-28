f25d60fe10bff2bb0fd7d277af6bd0f079f3d473
PC21 <PC21@PC21-PC>
2018-11-28 19:23:04 +0900

    commit

--- src/F_OOP2/AnnoInnerEx01.java
+++ src/F_OOP2/AnnoInnerEx01.java
@@ -1,5 +1,24 @@
 package F_OOP2;
 
-public class AnnoInnerEx01 {
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class AnnoInnerEx01 {
+	public static void main(String[] args) {
+		Button b = new Button();
+		ActionListener a = new EventHandler();
+		b.addActionListener(a);
+	}
+}
+
+class EventHandler implements ActionListener{
+		
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("버튼 눌림");
+		
+	}
+	
 }
